c85ddaf8c7dcbfd23d860aa8717f0f7f01eb35fb
Y2theK <yeyintkyaw300@gmail.com>
2022-04-07 14:16:26 -0700

    adding todo, deleting todo & showing list of todo is done

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,22 @@ const app = Vue.createApp({
   data() {
     return {
       appName: "Vue Todo App",
+      todo: "",
+      todolist: [],
     };
+  },
+  methods: {
+    addTodo() {
+      if (this.todo == "") {
+        alert("Enter todo and try again");
+        return;
+      }
+      this.todolist.push(this.todo);
+      this.todo = "";
+    },
+    removeTodo(index) {
+      this.todolist.splice(index, 1);
+      console.log(index);
+    },
   },
 });
